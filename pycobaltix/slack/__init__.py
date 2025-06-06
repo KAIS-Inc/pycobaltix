fb5075cc46716ba131801baf958a3a413c01ab62
Heesung <kinest1997@naver.com>
2025-06-06 11:17:47 +0900

    디펜던시 수정

--- pycobaltix/slack/__init__.py
+++ pycobaltix/slack/__init__.py
@@ -1,6 +1,9 @@
 from pycobaltix.slack.slack_web_hook import SlackWebHook, SlackBot
+from pycobaltix.slack.async_slack_web_hook import AsyncSlackWebHook, AsyncSlackBot
 
 __all__ = [
     "SlackWebHook",
     "SlackBot",
+    "AsyncSlackWebHook",
+    "AsyncSlackBot",
 ]
